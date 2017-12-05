a6def3809c3e6454836dde800ae131c7b5f4ed5f
EmilyW11 <31204525+EmilyW11@users.noreply.github.com>
2017-12-05 08:14:22 +0100

    theme changes

--- config/theme-selector.php
+++ config/theme-selector.php
@@ -6,7 +6,7 @@
 return [
 
     "separator" => "------------------------------------------------",
-    
+
     "themes" => [
 
         "separator0" => "------------------------------------------------",
@@ -15,15 +15,20 @@ return [
             "title"      => "Minimal style, only the plain base",
             "class"      => "",
             "stylesheets" => [
-                "css/modules.min.css"
+                "css/base.min.css", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
             ]
         ],
 
         "default"   => [
             "title"      => "Your own selected default theme",
-            "class"      => "",
+            "class"      => "light",
             "stylesheets" => [
-                "css/default.min.css"
+                "css/default.min.css",
+                "css/fun.min.css",
+                "https://fonts.googleapis.com/css?family=Bigelow+Rules%7CQuattrocento%7CComfortaa%7CCinzel%7CSpectral+SC%7CPoppins",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+                "https://fonts.googleapis.com/css?family=Julius+Sans+One%7CPhilosopher%7CSatisfy"
+
             ]
         ],
 
@@ -32,39 +37,60 @@ return [
         "light"     =>  [
             "title"      => "Very light theme, white, black and nuances of grey",
             "class"      => "light",
-            "stylesheets" => []
+            "stylesheets" => [
+                "css/light.min.css",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            ]
         ],
 
         "color"     => [
             "title"      => "Enhance the light theme by adding a tiny bit of color",
-            "class"      => "color",
-            "stylesheets" => []
+            "class"      => "light",
+            "stylesheets" => [
+                "css/color.min.css",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            ]
         ],
 
         "dark"      => [
             "title"      => "Dark background and light text",
             "class"      => "dark",
-            "stylesheets" => []
+            "stylesheets" => [
+                "css/dark.min.css",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            ]
         ],
 
         "colorful"  => [
             "title"      => "Make a very colorful theme",
-            "class"      => "colorful",
-            "stylesheets" => []
+            "class"      => "light",
+            "stylesheets" => [
+                "css/colorful.min.css",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            ]
         ],
 
         "typography" => [
             "title"      => "A theme where the typography really stands out",
             "class"      => "light",
-            "stylesheets" => []
+            "stylesheets" => [
+                "css/typo.min.css",
+                "https://fonts.googleapis.com/css?family=Bigelow+Rules%7CQuattrocento%7CComfortaa%7CCinzel%7CSpectral+SC%7CPoppins",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            ]
         ],
 
         "separator2" => "------------------------------------------------",
 
         "fun"       => [
             "title"      => "All fun, test and play, make it stand out!",
-            "class"      => "fun",
-            "stylesheets" => []
+            "class"      => "light",
+            "stylesheets" => [
+                "css/fun.min.css",
+                "https://fonts.googleapis.com/css?family=Bigelow+Rules%7CQuattrocento%7CComfortaa%7CCinzel%7CSpectral+SC%7CPoppins",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+                "https://fonts.googleapis.com/css?family=Julius+Sans+One%7CPhilosopher%7CSatisfy"
+            ]
         ],
     ]
 ];
